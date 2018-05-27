afbe1b6aaf30ee74a95aa6d6e2691c210ff0ad3d
Mohamed Khaled <mkhaled@codiles.com>
2018-05-28 00:37:25 +0200

    Add fpcalc

--- src/android/Fingerprint.java
+++ src/android/Fingerprint.java
@@ -3,14 +3,14 @@ package com.uz.plugin.fpcalc;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-
+import com.geecko.fpcalc.Fpcalc;
 public class Fingerprint extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("fingerprint")) {
-
+            Fpcalc.fpCalc(["test"]);
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
